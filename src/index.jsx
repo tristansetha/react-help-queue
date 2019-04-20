e281206356b43a6a6075821159ff5b863fd7a02f
ts <tristanasetha@gmail.com>
2019-04-20 14:21:44 -0700

    into jsx

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3a</h3>
+    <h3>Thato and Haley</h3>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
+
+// var heading = React.createElement('h1', {}, 'Help, Queue');
+// var ticketLocation = React.createElement('h3', {}, '3a');
+// var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
+// var ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
+// var app = React.createElement('div', {}, heading);
+
+// ReactDOM.render(
+// app, //what to render
+// document.getElementById('react-app-root') //where to render
+// );
+
+
+
