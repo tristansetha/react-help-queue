61d5ee108aed64ebe3b725b216f44ce32967be92
Guest User <Guest@epicodus-79.local>
2019-04-29 16:41:48 -0700

    updated

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -7,7 +7,7 @@ function TicketList(props){
   return(
     <div>
       <hr/>
-      {props.ticketList.map((ticket) =>
+      {props.ticketList.map((ticket) => 
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue} 
